ecc340f79ee9cc004e0e0865dc485c74eff2641e
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-05-18 10:29:25 +0200

    Update docstring in pascal_triangle function for clarity and formatting

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -3,7 +3,14 @@
 
 
 def pascal_triangle(n):
-    """Returns a list of lists of integers representing the Pascal’s triangle of n"""
+    """Return a list of lists of integers representing Pascal's triangle.
+
+    Args:
+        n (int): Number of rows of the triangle.
+
+    Returns:
+        list[list[int]]: Pascal's triangle of size n.
+    """
     if n <= 0:
         return []
     triangle = []
