e3260069d7ecc87bf1b4985a59c590c602235fdc
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-06-12 22:56:35 +0200

    Refine log regex for improved accuracy

---
+++ utf8_validation/0-validate_utf8.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Validates whether a data set represents a valid UTF-8 encoding."""
+
+
+def validUTF8(data):
+    """Determine if a given data set represents a valid UTF-8 encoding."""
+    num_bytes = 0
+
+    for byte in data:
+        byte = byte & 0xFF
+
+        if num_bytes == 0:
+            if byte >> 7 == 0:
+                num_bytes = 0
+            elif byte >> 5 == 0b110:
+                num_bytes = 1
+            elif byte >> 4 == 0b1110:
+                num_bytes = 2
+            elif byte >> 3 == 0b11110:
+                num_bytes = 3
+            else:
+                return False
+        else:
+            if byte >> 6 != 0b10:
+                return False
+            num_bytes -= 1
+
+    return num_bytes == 0
